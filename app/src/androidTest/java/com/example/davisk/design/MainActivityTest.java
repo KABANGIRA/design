0a83ca70b2a57457d4324893d82c952d63bdfee1
Ronald <rubahamyaronald@gmail.com>
2018-04-20 17:08:33 +0300

    Initial commit

---
+++ app/src/androidTest/java/com/example/davisk/design/MainActivityTest.java
@@ -0,0 +1,10 @@
+package com.example.davisk.design;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Ronald on 4/20/2018.
+ */
+public class MainActivityTest {
+
+}
---
+++ app/src/main/java/com/example/davisk/design/MyAdapter.java
@@ -0,0 +1,5 @@
+package com.example.davisk.design;
+
+import android.support.v7.widget.RecyclerView;
+
+
